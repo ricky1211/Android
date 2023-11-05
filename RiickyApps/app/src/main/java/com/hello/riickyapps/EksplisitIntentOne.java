afbec81227d7592459b3b596c6a8082f6d73a1a4
ricky1211 <rickyalfian751@gmail.com>
2023-11-05 18:16:57 +0700

    File Baru

---
+++ RiickyApps/app/src/main/java/com/hello/riickyapps/EksplisitIntentOne.java
@@ -0,0 +1,20 @@
+package com.hello.riickyapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EksplisitIntentOne extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+
+    public void newsScreen(View view) {
+        Intent i = new Intent(getApplicationContext(), EksplisitIntentTwo.class);
+        startActivity(i);
+    }
+}
